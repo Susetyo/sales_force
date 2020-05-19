ec0793ea9344d56f67f29c1fa5cabd665072f768
Susetya Mulia Pambudi <susetyamulia@gmail.com>
2020-05-19 15:19:07 +0700

    add configuration

--- api/getAccessToken.js
+++ api/getAccessToken.js
@@ -10,7 +10,8 @@ function accessToken() {
 
     this.getToken = async function () {
         const url =
-            "https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=" +
+            process.env.URL_SALES +
+            "oauth2/token?grant_type=password&client_id=" +
             this.consumerKey +
             "&client_secret=" +
             this.consumerSecret +
@@ -28,19 +29,10 @@ function accessToken() {
 
         let res = await response.json();
 
-        console.log(res);
+        return res;
     };
 }
 
 let objAccessToken = new accessToken();
 
-objAccessToken.getToken();
-
-// {
-//     access_token: '00D2x000003t1Qg!ARsAQGjJWVrbZrsFEZAZdWChNiqhD2gOu.MNhySOPw6ZKPcMzX5LsFTLr8EzKfYjxuLJvyKvY9zhTfkeEdbkfxjea9X4WMvl',
-//     instance_url: 'https://ap17.salesforce.com',
-//     id: 'https://login.salesforce.com/id/00D2x000003t1QgEAI/0052x000001XDDBAA4',
-//     token_type: 'Bearer',
-//     issued_at: '1589796579939',
-//     signature: 'sigoIXjhvoCrN9gUJ9WfbsbtwUebTjk0yL9pinvwIvw='
-//   }
+module.exports = objAccessToken;
--- api/getListAvailableEndPoint.js
+++ api/getListAvailableEndPoint.js
@@ -1,1 +1,18 @@
-function getList() {}
+require("dotenv").config();
+const fetch = require("node-fetch");
+const accessToken = require("./getAccessToken");
+
+function ListEndPoint() {
+    this.url = process.env.URL_SALES + "data/v26.0/";
+
+    this.getList = async function () {
+        const token = await accessToken.getToken();
+        const res = await fetch(this.url);
+        const result = await res.json();
+
+        return result;
+    };
+}
+
+const list = new ListEndPoint();
+console.log(list.getList());
